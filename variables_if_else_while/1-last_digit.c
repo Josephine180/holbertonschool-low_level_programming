21236dd83d8cc0f86c23cff4869424a1d8c43485
Josephine180 <pierre-josephine@hotmail.com>
2024-10-21 13:43:09 +0000

    de

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 int main(void)
 {
     int n;
-
+    int last_digit;
    
     srand(time(0));
 
@@ -13,7 +13,7 @@ int main(void)
     n = rand() - RAND_MAX / 2; 
 
   
-    int last_digit = n % 10;
+    last_digit = n % 10;
     
     if (last_digit < 0)
     {
