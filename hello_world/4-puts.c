11241053ea1a19ce97f5a7e8dfbfba25bc952d99
Josephine180 <pierre-josephine@hotmail.com>
2024-10-21 10:08:46 +0000

    d

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,7 @@
 #!/bin/bash
-gcc -o puts("Programming is like building a multilingual puzzle") ;
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
+    
