d9d5ff9c5b36c7aa7d05daf6bf10e0a321ae107d
Josephine180 <pierre-josephine@hotmail.com>
2024-10-21 11:18:01 +0000

    d

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #!/bin/bash
-gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
-ANS #include <stdio.h>
-
-* Return: Always 0 (Success)*/
-
+#include <stdio.h>
+/**
+*main - Entry point
+*Return : Always 0 (Success)
+*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle\n");
     return (0);
 }
     
