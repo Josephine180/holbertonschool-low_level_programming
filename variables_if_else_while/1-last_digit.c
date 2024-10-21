909c88d4fc6abbaa80f2b7a04613ac0e2fc96f9f
Josephine180 <pierre-josephine@hotmail.com>
2024-10-21 13:54:24 +0000

    betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,9 +9,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2; 
 last_digit = n % 10;
 /*if (last_digit < 0)
-{
-last_digit = -last_digit;
-}*/
+{last_digit = -last_digit;}*/
 printf("Last digit of %d is ", n);
 if (last_digit > 5)
 {
