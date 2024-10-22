730249bd3b494a2d496435330e282d7ed58fe024
Josephine180 <pierre-josephine@hotmail.com>
2024-10-22 08:28:28 +0000

    d

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - main
+*
+* Return: osef 
+*/
+
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+{
+putchar(alphabet);
+ if (alphabet == 'd')
+   alphabet++
+}
+{
+if (alphabet =='p')
+alphabet++
+}
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
