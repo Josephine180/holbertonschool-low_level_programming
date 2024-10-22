359a9d6e92eca0935c93490c60cf6383040cfb79
Josephine180 <pierre-josephine@hotmail.com>
2024-10-22 08:49:29 +0000

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - main
+*
+* Return: result
+*/
+
+int main(void)
+{
+int x;
+ for (x=0; x < 10; x ++);
+ {
+   printf ( "%d", x);
+   if (x < 9 )
+     {
+       printf(",");
+     }
+ }
+ printf ("\n");
+ return (0);
+}
+	 
