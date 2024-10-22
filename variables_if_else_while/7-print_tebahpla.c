7b937aaa1574e884c63207b580585f41666814ac
Josephine180 <pierre-josephine@hotmail.com>
2024-10-22 09:24:15 +0000

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - main
+*
+* Return: success
+*/
+
+int main(void)
+{
+char alphabet;
+for (alphabet = 'z'; alphabet >= 'a' ; alphabet--)
+{
+putchar('\n');
+}
+return (0);
+}
