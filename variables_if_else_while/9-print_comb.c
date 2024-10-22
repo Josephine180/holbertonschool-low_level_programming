6cc07f219a1dc9bbb65de92a47f7fa76badc787f
Josephine180 <pierre-josephine@hotmail.com>
2024-10-22 09:56:48 +0000

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,10 +10,15 @@
 int main(void)
 {
 int x;
-for (x = 0 ; x < 10 ; x++)
+for (x = 0; x < 10; x++)
 {
-putchar("%d", x);
+putchar(x + '0');
+if (x < 9)
+{
+putchar(',');
+putchar(' ');
 }
-putchar("\n");
+}
+putchar('\n');
 return (0);
 }
