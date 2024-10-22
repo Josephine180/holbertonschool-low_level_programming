9daf47579c513d1c689ea3eb5ebece3627744d09
Josephine180 <pierre-josephine@hotmail.com>
2024-10-22 13:04:35 +0000

    d

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+* main - writes the character c to stdout
+*
+*
+* Return: On success 1.
+*/
+
+void print_alphabet(void)
+{
+char letter;
+for( letter = 'a'; letter <='z'; letter++)
+{
+_putchar(letter);}
+ _putchar ('\n');
+}
