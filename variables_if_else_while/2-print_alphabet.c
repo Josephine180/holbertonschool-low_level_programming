f9fd4ddb8273c1db994274947366b37b85157b64
Josephine180 <pierre-josephine@hotmail.com>
2024-10-22 07:48:39 +0000

    test

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,21 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(void) {
-  int i;
-    char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-    for (i = 0; i < 26; i++) {
-        putchar(alphabet[i]);
-    }
-    putchar('\n');
-    return (0);
+
+/**
+* main - main
+*
+* Return: osef
+*/
+
+int main(void)
+{
+int i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+for (i = 0; i < 26; i++)
+{
+putchar(alphabet[i]);
+}
+putchar('\n');
+return (0);
 }
