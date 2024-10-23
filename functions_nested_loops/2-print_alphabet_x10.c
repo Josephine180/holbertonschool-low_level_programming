042b0bee15d9c31cde0cc2820d616c01488417c3
Josephine180 <pierre-josephine@hotmail.com>
2024-10-23 08:44:24 +0000

    d

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,11 +11,11 @@
 
 void print_alphabet_x10(void)
 {
+int i;
 char letter;
-
- for (int i = 0; i < 10; i++)
-	for(char letter = 'a'; letter <= 'z'; letter++)
+ for (i = 0; i <= 10; i++)
 {
+for(letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 }
 _putchar ('\n');
