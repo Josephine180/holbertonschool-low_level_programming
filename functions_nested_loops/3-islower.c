381f5bfb3c15e35a4859d51b59bccd0f03030b7e
Josephine180 <pierre-josephine@hotmail.com>
2024-10-23 09:10:15 +0000

    d

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* main - main 
+* Description : description
+* Return: success
+*/
+
+int _islower(int c)
+{
+if ( c >= 'a' && c <= 'z')
+{ return 1; }
+else
+return 0;
+}
+
+
