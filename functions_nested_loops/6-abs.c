e3fe5d0124432050fb36b7d104082c764265b6b1
Josephine180 <pierre-josephine@hotmail.com>
2024-10-23 12:07:18 +0000

    d

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* print_sign - check the code
+* @n: The number to test
+*
+* Return: 1 if n is greater than 0
+* 0 if n = 0 or -1 if less than 0.
+*/
+
+int _abs(int)
+{
+  int abs(n);
+  return (0);
+}
+ 
