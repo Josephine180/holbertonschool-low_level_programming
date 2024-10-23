7f11b29778888d574eab2dbdc1a07d54a7e71ace
Josephine180 <pierre-josephine@hotmail.com>
2024-10-23 14:12:42 +0000

    d

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,22 +18,23 @@ for (i=0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {    
-result = i * j;
- if (result < 10 )
+ if (i *j > 9)
+   {
+     _putchar(i * j / 10 + '0');
+    
+     else if ( j > 0 )
    {
      _putchar(' ');
-     _putchar(result + '0');
-   }
- else
-   {
-     _putchar((result / 10) + '0');
-     _putchar((result % 10) + '0');
+     _putchar(i * j % 10 + '0');
    }
  if (j < 9)
    {
      _putchar(',');
+     _putchar(' ');
+   }
    }
    _putchar('\n');
    }
-
  }
+
+
