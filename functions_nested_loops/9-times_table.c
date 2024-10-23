121c500130c88d71ff365aae4f96f05d27cffd9a
Josephine180 <pierre-josephine@hotmail.com>
2024-10-23 13:55:15 +0000

    d

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,19 +12,28 @@ void times_table(void)
 {
 int i;
 int result;
-
+int j;
+ 
 for (i=0; i <= 9; i++)
 {
-result = 9*i;
- if (i == 0)
-{
-_putchar(result);
-}
-else
-{
-  _putchar(',');
-  _putchar(result);
-}
-}
-_putchar('\n');
-}
+for (j = 0; j <= 9; j++)
+{    
+result = j * i;
+ if (result < 10 )
+   {
+     _putchar(' ');
+     _putchar(result + '0');
+   }
+ else
+   {
+     _putchar((result / 10) + '0');
+     _putchar((result % 10) + '0');
+   }
+ if (j < 9)
+   {
+     _putchar(',');
+     _putchar(' ');
+   }
+ }
+ _putchar('\n');
+ }
