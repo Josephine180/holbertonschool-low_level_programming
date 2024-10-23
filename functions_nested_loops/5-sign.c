ba15289e9feb5edcb7f65e639e4dbb9edafe9a19
Josephine180 <pierre-josephine@hotmail.com>
2024-10-23 11:19:09 +0000

    d

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/** 
+* print_sign - check
+* @c : caractere check
+* Description : osef
+* Return: success
+*/
+
+int print_sign(int n)
+{
+if (n > '0')
+{
+putchar('+');  
+return (1);
+}
+else if (n == '0')
+{
+putchar('0');  
+return (0);
+}
+ else
+   {
+     putchar (' ');
+    return (-1);
+}
+}
