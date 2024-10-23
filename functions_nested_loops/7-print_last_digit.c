bde97a4b7ad5e64cf0e6dbd1c53f8ce216c1cbdd
Josephine180 <pierre-josephine@hotmail.com>
2024-10-23 12:30:33 +0000

    d

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+* print_last_digit - check the code
+* @n: The number to test
+*
+* Return: 1 if n is greater than 0
+* 
+*/
+
+int print_last_digit(int n)
+{
+int last_digit = n % 10;
+ if (last_digit < 0)
+        {
+	  last_digit = -last_digit;
+        }
+ char digit_char = last_digit + '0';
+ write(1,&digit_whar, 1);
+ return last_digit;
+}
