455511e6f68b275cb3f0a465a0ca648dd32ee7bc
Josephine180 <pierre-josephine@hotmail.com>
2024-10-23 13:14:41 +0000

    d

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,19 +2,18 @@
 #include <stdio.h>
 
 /**
- * print_sign - check the code
- * @n: The number to test
- *
- * Return: 1 if n is greater than 0
- * 0 if n = 0 or -1 if less than 0.
- */
+* print_sign - check the code
+* @n: The number to test
+*
+* Return: 1 if n is greater than 0
+* 0 if n = 0 or -1 if less than 0.
+*/
 
 void jack_bauer(void)
 {
 int hour, minute;
- for ( hour = 0; hour <=23; hour++)
-{
-  for ( minute = 0; minute <= 59 ; minute++)
+for (hour = 0; hour <= 23; hour++)
+for (minute = 0; minute <= 59 ; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
