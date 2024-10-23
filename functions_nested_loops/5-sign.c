cee4fd6d77752cdd4f7393dfc16cf6e3fc765cd5
Josephine180 <pierre-josephine@hotmail.com>
2024-10-23 11:28:41 +0000

    d

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,12 +13,12 @@ int print_sign(int n)
 {
 if (n > '0')
 {
-write(1, "+" ,1);  
+fwrite (1, "+" ,1);  
 return (1);
 }
 else if (n == '0')
 {
-write(1,'0',1);  
+fwrite(1,'0',1);  
 return (0);
 }
  else
