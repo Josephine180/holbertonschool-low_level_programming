ec903b7ca30f9c9487d1e8d5c0cfda158089c66e
Josephine180 <pierre-josephine@hotmail.com>
2024-10-25 08:34:36 +0000

    d

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,19 +9,22 @@
 * Return: nombres jusqu'a 98
 */
 
+
 void print_to_98(int n)
-{  if (n < 98 )
 {
-  for (; n <= 98; n++)
-    {
-      printf("%d, ", n);
-    }
- }
-  {  if ( n > 98)
-      { for(; n > 98; n--)
-    {
-      printf("%d, ", n);
-    }
-      }
-  putchar('\n');
+if (n < 98)
+{
+for (; n < 98; n++)
+{
+printf("%d, ", n);
+}
+}
+else
+{
+for (; n > 98; n--)
+{
+printf("%d, ", n);
+}
+}
+printf("98\n");
 }
