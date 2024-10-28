444bcd0f354653e1156a3c1693c8f58683a4c665
Josephine180 <pierre-josephine@hotmail.com>
2024-10-28 17:33:32 +0000

    d

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,25 +7,15 @@
 *
 * Return: 0 a 9 sans 2 et 4 
 */
-/**
+
+
 void print_most_numbers(void)
 {
-for(int i = 0; i <= 9; i++)
+int i;
+for( i = 0; i != 2 && i != 4; i <= 9; i++)
 {
-if ( i != 2 && i != 4)
-{
-_putchar(i + '0');
-}
+_putchar(i);
 }
 _putchar('\n');
 }
-*/
 
-void print_most_numbers(void) {
-    for (int i = 0; i <= 9; i++) { // Loop from 0 to 9
-        if (i != 2 && i != 4) {     // Skip 2 and 4
-            _putchar(i + '0');      // Convert int to char and print
-        }
-    }
-    _putchar('\n'); // Print a newline character
-}
