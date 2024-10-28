3183aced2b6be9736a31851fcc71783ac535d430
Josephine180 <pierre-josephine@hotmail.com>
2024-10-28 13:13:43 +0000

    test

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -9,12 +9,15 @@
 */
 
 int _isupper(int c);
-if( i = [' A-Z'])
 {
-  _putchar('1');
+if( c >= 'A' && c <= 'Z' )
+{
+_putchar('1');
+return(1); 
 }
 else
 {
  _putchar('0');
- return (0);
- }
+ return(0);
+}
+}
