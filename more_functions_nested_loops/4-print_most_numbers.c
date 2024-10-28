ef4507de9a46f534b20bf975aa72bc2ebd8a7055
Josephine180 <pierre-josephine@hotmail.com>
2024-10-28 14:50:57 +0000

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* print_most_numbers - print de 0 a 9 sans 2 et 4
+*
+* Return: 0 a 9 sans 2 et 4 
+*/
+
+void print_most_numbers(void)
+{
+int i;
+for (i=0; i != '2' && i != '4'; i <= '9'; i++)
+{
+_putchar(i);
+}
+}
