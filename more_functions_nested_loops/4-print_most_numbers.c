626ca76da064c57484781e3c3b231aff945fd462
Josephine180 <pierre-josephine@hotmail.com>
2024-10-28 16:43:21 +0000

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,12 @@
 void print_most_numbers(void)
 {
 int i;
-for (i=0; i != '2' && i != '4'; i <= '9'; i++)
+for ( i = 0; i <= '9'; i++)
 {
-_putchar(i);
+if( i != '2' && i != '4')
+{
+  _putchar(i + '0');
 }
+}
+ putchar ('\n');
 }
