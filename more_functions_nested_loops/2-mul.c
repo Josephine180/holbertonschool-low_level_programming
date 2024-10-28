977c6e1c14ccbb052f7e5b8e4093911f3fc7e0e9
Josephine180 <pierre-josephine@hotmail.com>
2024-10-28 14:02:16 +0000

    d

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - multiplication de deux integrateurs
+* @a : caractere 1
+* @b : caractere 2
+* Return: 0
+*/
+
+int a;
+int b; 
+
+int mul(int a, int b)
+{
+return ( a* b);
+}
