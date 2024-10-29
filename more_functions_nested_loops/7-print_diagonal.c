df55f001a9e54282046e885e587a0641dd8a2341
Josephine180 <pierre-josephine@hotmail.com>
2024-10-29 09:16:07 +0000

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_diagonal - faire une ligne de \
+* @n : caractere 
+* Result : n inferieur a 0 = '\n'
+*/
+
+void print_diagonal(int n)
+int i;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+_putchar('\');
+}
+_putchar('\n');
+}
+}
