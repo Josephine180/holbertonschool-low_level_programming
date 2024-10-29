38657860506fb7b31fd22b21b93b301c78e9eb18
Josephine180 <pierre-josephine@hotmail.com>
2024-10-29 10:16:57 +0000

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* print_square - function that prints a square
+* @size : size of the square
+*
+* Return : result
+*/
+
+void print_square(int size)
+{
+int j;
+int i;
+  
+if (size <= 0)
+{
+_putchar('\n');
+}
+for (i = 0; i < size; i++)
+{
+for (j =0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
