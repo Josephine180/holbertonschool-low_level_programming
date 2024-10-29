1dfe3ec6f9d6b606fa9d1d500475e9aca0eaa33c
Josephine180 <pierre-josephine@hotmail.com>
2024-10-29 09:38:22 +0000

    d

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,10 @@
 */
 
 void print_diagonal(int n)
+{
 int i;
+int j;
+ 
 if (n <= 0)
 {
 _putchar('\n');
@@ -17,7 +20,11 @@ else
 {
 for (i = 0; i < n; i++)
 {
-_putchar('\');
+for (j=0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar ('\\');
 }
 _putchar('\n');
 }
