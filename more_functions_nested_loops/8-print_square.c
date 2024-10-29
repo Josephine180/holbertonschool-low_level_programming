32cab828d3a7a3fd7297ab0712bff87b3ae72fa3
Josephine180 <pierre-josephine@hotmail.com>
2024-10-29 10:20:14 +0000

    d

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,14 +12,13 @@ void print_square(int size)
 {
 int j;
 int i;
-  
 if (size <= 0)
 {
 _putchar('\n');
 }
 for (i = 0; i < size; i++)
 {
-for (j =0; j < size; j++)
+for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
