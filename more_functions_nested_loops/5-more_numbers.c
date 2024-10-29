ab3b5331604eb65abebd8e2f72afdf63f42e1466
Josephine180 <pierre-josephine@hotmail.com>
2024-10-29 08:40:42 +0000

    d

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,6 @@ void more_numbers(void)
 int i, j; 
 for (i  = 0; i < 10; i++)
 {
-for (j = 0; j <= 14; j++)
-{
-if (j >= 10)
-{
-_putchar('1');
- _putchar ((j % 10) + '0'));
+write(1, "01234567891011121314\n", 21);
 }
-_putchar('\n');
- }
- }
+}
