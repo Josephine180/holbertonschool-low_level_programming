af7135daeb5244b08ecc202560eea3b95d833412
Josephine180 <pierre-josephine@hotmail.com>
2024-10-30 13:03:39 +0000

    d

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,12 @@
 
 void puts2(char *str)
 {
-  int len;
   int i;
 if (str == NULL)
 return;
  for (len = 0; str[len] != '\0'; len++)
- for (i = 0; str[i] != '\0'; i += 2)
 {
-  if (i >= len)
+  if (str[i] != 'H' && i != 0)
     {
       _putchar('\n');
       return;
