1010c5076cdc2fe6f5c2768619fa8e039c2c2b3a
Josephine180 <pierre-josephine@hotmail.com>
2024-10-30 18:20:46 +0000

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* *_strcpy - print a function 
+* 
+* Result: string
+*/
+
+char *_strcpy(char *dest, char *src);
+{
+*ptr = dest;
+while (*src != '0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return ptr;
+}
