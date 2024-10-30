aad69c82c2c603530799d6e3e92b5599a3bb9ca0
Josephine180 <pierre-josephine@hotmail.com>
2024-10-30 14:56:09 +0000

    d

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,30 +1,22 @@
 #include "main.h"
 
-/*
-* puts_half - print the half of the string
-* @str: pointer
-* Return: half string
-*/
-
+/**
+ * puts_half - Prints the second half of a string, followed by a new line.
+ * @str: Pointer to the string to print.
+ */
 void puts_half(char *str)
 {
-	int len;
-	/* declarer variable pour longueur de la string */
-	int i;
+	int i, length = 0;
 
-	for (len = 0; str[len] != '\0'; len ++)
-	/* taille totale de string str */
-	if (len % 2 == 0 )
-		i = len / 2;
-	/* i divise  par 2 si cest pair */
+	while (str[length] != '\0')
+		length++;
+	if (length % 2 == 0)
+		i = length / 2;
 	else
-		i = (len - 1) / 2;
-	/* on enleve 1 si c'est impair, et on ajoute 1 */
-	/* 9  moins 1 8 divise par 2 4 + 1 5*/
+		i = (length - 1) / 2 + 1;
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
 	}
-	_putchar('\n');
 }
