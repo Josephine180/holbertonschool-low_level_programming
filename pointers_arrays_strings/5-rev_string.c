52cc07ca3d1160dc3dad0f6c706aade43155fe46
Josephine180 <pierre-josephine@hotmail.com>
2024-10-30 11:14:28 +0000

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,19 +9,18 @@ File Edit Options Buffers Tools C Help
 
 void rev_string(char *s)
 {
-int left = 0;
-int right = 0;
-while (s[right] !='\0')
+char temp; // declaration variable temporaire de stockage
+char *ptr = s; // creation d'un deuxieme pointeur de la meme valeur que le premier
+while(*ptr) // creation boucle
+ptr++; // le pointeur 2 va a la fin de la string
+ptr--; // decrementer pour avoir le dernier caractere de la string
+while (s <= ptr) // on veut que les pointeurs s'arretent et ne se croisent pas
 {
-right++;
+char temp = *s; // on fait un switch des deux valeurs des adresses des pointeurs
+*s = *ptr; // pointeur de gauche prend la valeur de l'adresse contenue du pointeur de droite. 
+*ptr = temp; // pointeur de droite prend la valeur contenue dans la variable de stockage
+ptr--; // decremente le pointeur de droite (en gros aller sur la gauche ) 
+s++; // incremente le pointeur de gauche pour aller a droite ( les pointeurs se rejoignent)
+}  
 }
-right--;
-while (left < right)
-{
-char temp = s[left];
-s[left] = s[right];
-s[right] = temp;
-left++;
-right--;
-}
-}
+ 
