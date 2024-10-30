4bcb6921e9601bdf4ba0e9b26062056c6d05998b
Josephine180 <pierre-josephine@hotmail.com>
2024-10-30 13:02:26 +0000

    d

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,18 @@
 
 void puts2(char *str)
 {
+  int len;
   int i;
 if (str == NULL)
 return;
+ for (len = 0; str[len] != '\0'; len++)
  for (i = 0; str[i] != '\0'; i += 2)
 {
+  if (i >= len)
+    {
+      _putchar('\n');
+      return;
+    }
 _putchar(str[i]);
 }
 _putchar('\n');
