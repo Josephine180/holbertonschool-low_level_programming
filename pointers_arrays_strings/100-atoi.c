b061613254e291eeb7e463739f2e26b04c71dea4
Josephine180 <pierre-josephine@hotmail.com>
2024-10-30 19:19:30 +0000

    d

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,6 +4,7 @@ int _atoi(char *s)
 {
 int sign = 1; 
 unsigned int number = 0;    
+unsigned int limit = 2147483647;
 while (*s)
 {
 if (*s == ' ')
@@ -22,7 +23,7 @@ s++;
 }
 while (*s >= '0' && *s <= '9')
 {
-if (number > (2147483647 - (*s - '0')) / 10)
+if (number > (limit - (*s - '0')) / 10)
 {
 return (sign == 1) ? 2147483647 : -2147483648;
 }
