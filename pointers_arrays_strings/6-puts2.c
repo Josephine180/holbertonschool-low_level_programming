0bd1a52e36135cbe8ac96e06274efd7cd8ef3a98
Josephine180 <pierre-josephine@hotmail.com>
2024-10-30 13:19:27 +0000

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,22 +3,22 @@
 
 /**
 * puts2 - print every other character of a string
-* @str - pointeur
+* @str: pointeur
 * Result: success
 */
 
 void puts2(char *str)
 {
-  int i;
+int i;
 if (str == NULL)
 return;
- for (i = 0; str[i] != '\0'; i += 2)
+for (i = 0; str[i] != '\0'; i += 2)
 {
-  if (str[i] == 'H' && i != 0)
-    {
-      _putchar('\n');
-      return;
-    }
+if (str[i] == 'H' && i != 0)
+{
+_putchar('\n');
+return;
+}
 _putchar(str[i]);
 }
 _putchar('\n');
