78b30398377363dca21c0b9ec69c8e435a7d9503
Josephine180 <pierre-josephine@hotmail.com>
2024-10-30 14:41:46 +0000

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/*
+* puts_half - print the half of the string
+* @str: pointer
+* Return: half string
+*/
+
+void puts_half(char *str)
+{
+	int len;
+/* declarer variable pour longueur de la string */
+	int i;
+	for (len = 0; str[len] != '\0'; len ++)
+/* taille totale de string str */
+if (len  % 2 == 0 )
+i = len / 2;
+/* i divise  par 2 si cest pair */
+else
+i = (len - 1) / 2 + 1;
+/* on enleve 1 si c'est impair, et on ajoute 1 */
+/* 9  moins 1 8 divise par 2 4 + 1 5*/
+while (i <= len)
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
