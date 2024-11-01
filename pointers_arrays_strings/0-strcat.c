3977450e135b1d7e760dcd1dbbf65d6ca1336b67
Josephine180 <pierre-josephine@hotmail.com>
2024-11-01 13:39:38 +0000

    test2

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,15 +13,15 @@ char *_strcat(char *dest, char *src)
 {
 char  *ptr;
 ptr = dest;
+ while (*dest != '\0')
+   {
+     dest ++;
+   }
  while (*src != '\0')
    {
      *dest = *src;
      dest++;
      src++;
-   }
- while (*dest != '\0')
-   {
-     dest ++;
    }
  *dest = '\0';
  return ptr;
