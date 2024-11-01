8cd99aa226f42bf375acb7b24ec28516369c0741
Josephine180 <pierre-josephine@hotmail.com>
2024-11-01 17:59:24 +0000

    test

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* *leet - change letter in number
+* @str: pointer
+* Result: leet in number
+*/
+
+char *leet(char *str)
+{
+  char *ptr = str;
+
+ while (*ptr) {
+        
+        if (*ptr == 'a' || *ptr == 'A') {
+            *ptr = '4';
+        } else if (*ptr == 'e' || *ptr == 'E') {
+            *ptr = '3';
+        } else if (*ptr == 'o' || *ptr == 'O') {
+            *ptr = '0';
+        } else if (*ptr == 't' || *ptr == 'T') {
+            *ptr = '7';
+        } else if (*ptr == 'l' || *ptr == 'L') {
+            *ptr = '1';
+        }
+        ptr++; 
+    }
+
+    return (str); 
+}
--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -5,11 +5,14 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    char *str;
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
 
-    str = "0123456789";
-    puts_half(str);
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
     return (0);
 }
