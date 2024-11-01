5249e9d78df6590e0f72cb65f117493794d82991
Josephine180 <pierre-josephine@hotmail.com>
2024-11-01 15:47:34 +0000

    d

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,19 +12,20 @@
 
 
 char *_strncpy(char *dest, char *src, int n)
-{  char *ptr = dest; 
-    while (n > 0 && *src != '\0')
-      {
-        *dest = *src;
-        dest++;
-        src++;
-        n--;          
-    }
-while (n > 0) {
-        *dest = '\0';
-        dest++;
-        n--;
-    }
-
- return (ptr);
+{
+char *ptr = dest; 
+while (n > 0 && *src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+n--;          
+}
+while (n > 0)
+{
+*dest = '\0';
+dest++;
+n--;
+}
+return (ptr);
 }
