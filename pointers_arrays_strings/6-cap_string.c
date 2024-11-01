f093d215bdfd2214518e4cd493624b59a6172b99
Josephine180 <pierre-josephine@hotmail.com>
2024-11-01 17:38:57 +0000

    d

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -5,26 +5,28 @@
 /**
 * *cap_string - capitalize letters
 * @str: pointer
-* Return: 0 
+* Return: 0
 */
-  
-
-char *cap_string(char *str) {
-    int capitalize = 1; 
-    char separators[] = " \t\n,;.!?\"(){}";
+char *cap_string(char *str)
+{
+int capitalize = 1;
+char separators[] = " \t\n,;.!?\"(){}";
 char *ptr = str;
 int i;
-
-    for (*ptr = *str; *ptr != '\0'; ptr++) {
-        if (capitalize && *ptr >= 'a' && *ptr <= 'z') {
-            *ptr -= 32;
-        }
-        capitalize = 0;
-for (i = 0; separators[i] != '\0'; i++) {
-            if (*ptr == separators[i]) {
-                capitalize = 1;
+for (*ptr = *str; *ptr != '\0'; ptr++)
+{
+if (capitalize && *ptr >= 'a' && *ptr <= 'z')
+{
+*ptr -= 32;
 }
-        }
-    }
-    return str; 
+capitalize = 0;
+for (i = 0; separators[i] != '\0'; i++)
+{
+if (*ptr == separators[i])
+{
+capitalize = 1;
+}
+}
+}
+return (str);
 }
