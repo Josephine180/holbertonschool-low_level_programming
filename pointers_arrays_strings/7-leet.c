8903397c69e62ad2740a98bd2a247d061da9ad67
Josephine180 <pierre-josephine@hotmail.com>
2024-11-01 18:04:48 +0000

    9

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -5,28 +5,35 @@
 /**
 * *leet - change letter in number
 * @str: pointer
-* Result: leet in number
+* Return: leet in number
 */
 
 char *leet(char *str)
 {
-  char *ptr = str;
-
- while (*ptr) {
-        
-        if (*ptr == 'a' || *ptr == 'A') {
-            *ptr = '4';
-        } else if (*ptr == 'e' || *ptr == 'E') {
-            *ptr = '3';
-        } else if (*ptr == 'o' || *ptr == 'O') {
-            *ptr = '0';
-        } else if (*ptr == 't' || *ptr == 'T') {
-            *ptr = '7';
-        } else if (*ptr == 'l' || *ptr == 'L') {
-            *ptr = '1';
-        }
-        ptr++; 
-    }
-
-    return (str); 
+char *ptr = str;
+while (*ptr)
+{
+if (*ptr == 'a' || *ptr == 'A')
+{
+*ptr = '4';
+}
+else if (*ptr == 'e' || *ptr == 'E')
+{
+*ptr = '3';
+}
+else if (*ptr == 'o' || *ptr == 'O')
+{
+*ptr = '0';
+}
+else if (*ptr == 't' || *ptr == 'T')
+{
+*ptr = '7';
+}
+else if (*ptr == 'l' || *ptr == 'L')
+{
+*ptr = '1';
+}
+ptr++;
+}
+return (str);
 }
