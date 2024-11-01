b991df4fedc4cef8d663661c3ccc39a4c052acd8
Josephine180 <pierre-josephine@hotmail.com>
2024-11-01 17:00:34 +0000

    d

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -3,25 +3,21 @@
 #include <string.h>
 
 /**
-* *string_toupper
-* @*: pointers
+* *string_toupper - convert lowercase in uppercase
+* @ptr: pointers
 * Return: minuscule en majuscule
 */
 
 char *string_toupper(char *ptr)
 {
-  char *original = ptr;
-  while (*ptr)
-    {
-      if (*ptr >= 'a' && *ptr <= 'z')
-	{
-	  *ptr -= 32;
-	}
-  if(*ptr >= 'A' && *ptr <= 'Z')
-    {
-      *ptr += 32;
+char *original = ptr;
+while (*ptr)
+{
+if (*ptr >= 'a' && *ptr <= 'z')
+{
+*ptr -= 32;
 }
-ptr ++;
+ptr++;
 }
 return (original);
 }
