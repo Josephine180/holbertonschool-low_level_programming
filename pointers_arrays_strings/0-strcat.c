a8d7e6b46379de07a4783a385a59d489d6005007
Josephine180 <pierre-josephine@hotmail.com>
2024-11-01 13:46:41 +0000

    code bon

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-/** 
+/**
 * _strcat - the function concatenates two strings
 * @dest: pointeur destination
 * @src: pointeur source
@@ -13,16 +13,20 @@ char *_strcat(char *dest, char *src)
 {
 char  *ptr;
 ptr = dest;
- while (*dest != '\0')
-   {
-     dest ++;
-   }
- while (*src != '\0')
-   {
-     *dest = *src;
-     dest++;
-     src++;
-   }
- *dest = '\0';
- return ptr;
+/* creation pointeur*/
+while (*dest != '\0')
+/* tant que dest n'est pas a la fin il avance */ 
+{
+est++;
+}
+while (*src != '\0')
+/* en c = assigne une valeur, donc on copie src dans dest */
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return ptr;
+}
 }
