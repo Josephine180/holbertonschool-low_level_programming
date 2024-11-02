50269e45118092c7fe71c78ff1df3421d52e527e
Josephine180 <pierre-josephine@hotmail.com>
2024-11-02 15:04:24 +0000

    d

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include <math.h>
 
-long largest_prime_factor(long n) {
-    long max_prime = -1;
-	long i;
+/**
+* largest_prime_factor - value of factorisation
+* @n: pointers
+* Result: number
+*/
 
-    while (n % 2 == 0) {
-        max_prime = 2;
-        n /= 2;
-    }
-
-    for (i = 3; i <= sqrt(n); i += 2) {
-        while (n % i == 0) {
-            max_prime = i;
-            n /= i;
-        }
-    }
-    if (n > 2) {
-        max_prime = n;
-    }
-
-    return max_prime;
+long largest_prime_factor(long n)
+{
+long max_prime = -1;
+long i;
+while (n % 2 == 0)
+{
+max_prime = 2;
+n /= 2;
+}
+for (i = 3; i <= sqrt(n); i += 2)
+{
+while (n % i == 0)
+{
+max_prime = i;
+n /= i;
+}
+}
+if (n > 2)
+{
+max_prime = n;
+}
+return (max_prime);
 }
 
-int main() {
-    long number = 612852475143;
-    long result = largest_prime_factor(number);
-    printf("%ld\n", result);
-    return 0;
+int main()
+{
+long number = 612852475143;
+long result = largest_prime_factor(number);
+printf("%ld\n", result);
+return (0);
 }
