bbcaaf656f640f93ff4b7d7de8ee95468493d777
Josephine180 <pierre-josephine@hotmail.com>
2024-11-02 14:51:53 +0000

    d

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+long largest_prime_factor(long n) {
+    long max_prime = -1;
+
+
+    while (n % 2 == 0) {
+        max_prime = 2;
+        n /= 2;
+    }
+
+    for (long i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            max_prime = i;
+            n /= i;
+        }
+    }
+    if (n > 2) {
+        max_prime = n;
+    }
+
+    return max_prime;
+}
+
+int main() {
+    long number = 612852475143;
+    long result = largest_prime_factor(number);
+    printf("%ld\n", result);
+    return 0;
+}
