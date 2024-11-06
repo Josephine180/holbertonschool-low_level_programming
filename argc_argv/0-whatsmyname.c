e118ef6d9dde3b359f172533d873266c4050935b
Josephine180 <pierre-josephine@hotmail.com>
2024-11-06 14:45:18 +0100

    d

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
+/**
 * main - main
 * @argc: argument 1
 * @argv: argument 2
@@ -12,5 +12,8 @@ int main(int argc, char **argv)
 {
 printf("%s\n", argv[0]);
 if (argc == '\0')
+{
 return (0);
+}
+return ('\n');
 }
