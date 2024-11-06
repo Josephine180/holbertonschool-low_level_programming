d7d0914ec59e81e7459da1a737ebc359174bbd67
Josephine180 <pierre-josephine@hotmail.com>
2024-11-06 11:47:31 +0100

    d

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,19 +4,37 @@
 /**
  * is_prime_number - input integer is a prime number
  * @n: parameter number must be a prime
- * Return: 1 if it is or 0 if it's not.
+ * Return: 1 if it is or 0 if it's not
  */
 
 int is_prime_number(int n)
 {
-int i;
-for (i = 2; i <= n; i++)
+/* Handle base cases */
+if (n <= 1)
 {
-if (n % i == 0)
-break;
-}
-if (n == i)
-return (1);
-else
 return (0);
+/* Numbers <= 1 are not prime*/
+}
+if (n == 2)
+{
+return (1);
+/*2 is prime*/
+}
+if (n % 2 == 0)
+{
+return (0);
+/*Even numbers greater than 2 are not prime*/
+}
+/* Check for divisibility up to sqrt(n)*/
+int limit = (int) sqrt(n);
+int i;
+for (i = 3; i <= limit; i += 2)
+{
+/* Only check odd numbers */
+if (n % i == 0)
+{
+return (0);
+}
+}
+return (1);
 }
