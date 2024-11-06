622b3911a3058784839c4fe921996e23a0753016
Josephine180 <pierre-josephine@hotmail.com>
2024-11-06 14:51:32 +0100

    test

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-* @argc: nbr argument
-* @argv: aray argument
-* Return: 0 (success)
-*/
+ * main - Entry point
+ * @argc: number of arguments ?
+ * @argv: aray of arguments
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char **argv)
 {
 printf("%s\n", argv[0]);
 if (argc == '\0')
-printf("argc is used");
+printf("argc is used ?");
 return (0);
 }
