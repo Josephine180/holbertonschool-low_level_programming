6c219e0e199fafbfd1f66762500e970eadfab2e4
Josephine180 <pierre-josephine@hotmail.com>
2024-11-06 11:21:17 +0100

    test

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -11,8 +11,19 @@
 int _pow_recursion(int x, int y)
 {
 if (y == 0)
+/* resultat est 1 (puissance) */
 return (1);
 if (y < 0)
+/* programme retourne -1 */
 return (-1);
+/* y > 0 recursion en * x par lui m y fois*/
 return (x * _pow_recursion(x, y - 1));
+/* si x=2 et y=3*/
+/* ex: pow_recursion(2,3)=2*_pow(2,2)*/
+/* pow_recursion(2,2)= 2*_pow_rec(2,1)*/
+/*etc*/
+/* apres on remonte */
+/* pow recursion(2,1) retourne 2*1=2*/
+/*pow_recursion(2,2) retourne 2*2=4*/
+/*pow_recursion(2,3) retourne 2*4=8*/
 }
---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt_recursion_helper - helper
+ * @n: 1 parametre
+ * Return: if n have a natural square -1
+ */
+
+int _sqrt_recursion_helper(int n, int x)
+{
+/* cas de base: si x*x==n, c la racine carrée*/
+if (x * x == n)
+return (x);
+/* cas ou x*x depasse n* pas de racine carrée*/
+if (x * x > n)
+return (-1);
+/* on appelle recursivement */
+return (_sqrt_recursion_helper(n, x + 1));
+}
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+return (_sqrt_recursion_helper(n, 0));
+}
