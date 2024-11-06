07dd5b0a6ddd9297806cc281e84dcbf98db986ca
Josephine180 <pierre-josephine@hotmail.com>
2024-11-06 10:22:05 +0100

    betty

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,7 +4,7 @@
 /**
 * _strlen_recursion - mesure la taille de la string
 * @s: pointeur de debut
-* Result: longueur de la string
+* Return: longueur de la string
 */
 
 int _strlen_recursion(char *s)
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s)
 if (*s == '\0')
 /* fin de la string donc result est 0*/
 {
-return(0);
+return (0);
 }
-return(1) + _strlen_recursion(s + 1);
+return (1) + _strlen_recursion(s + 1);
 }
