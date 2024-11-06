a7d6a9aa2733a21a97c9061bd3fceba04c72a86c
Josephine180 <pierre-josephine@hotmail.com>
2024-11-06 14:53:46 +0100

    test

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -9,8 +9,8 @@
 
 int main(int argc, char **argv)
 {
-printf("%s\n", argv[0]);
-if (argc == '\0')
-printf("argc is used ?");
-return (0);
+        printf("%s\n", argv[0]);
+        if (argc == '\0')
+                printf("argc is used ?");
+        return (0);
 }
