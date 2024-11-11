2a63d3c53a4b5bc4643666c5c627045406e1023d
Josephine180 <pierre-josephine@hotmail.com>
2024-11-11 11:19:47 +0100

    1ere tache

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* _strdup - renvoi un pointeur sur nouvelle chaine
+* @str: string
+* Return: Null si str null, oui pointers
+*/
+
+char *_strdup(char *str)
+{
+int a = 0, i = 1;
+char *s;
+if (str == NULL)
+/* str est null donc on retourne null */
+return (NULL);
+while (str[i])
+/* on compte la longueur de la chaine */
+{
+i++;
+}
+s = malloc((sizeof(char) * 1) + 1);
+/* alloue la memoire pour la chaine */
+if (s == NULL)
+return (NULL);
+while (a < i)
+/* copie la memoire la chaine allouée */
+{
+s[a] = str[a];
+a++;
+}
+s[a] = '\0';
+return (s);
+}
