28cd29e89ab6f5aeb0d546e840ff285f8d325602
Josephine180 <pierre-josephine@hotmail.com>
2024-11-11 11:46:11 +0100

    test 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,8 +13,9 @@
 char *str_concat(char *s1, char *s2)
 {
 char *result;
+/* declaration pointeur chaine concatenée*/
 int len1, len2;
-
+/*variables pr stocker longueur chaines*/
 if (s1 == NULL)
 return (NULL);
 /*treat as empty string */
@@ -22,12 +23,17 @@ if (s2 == NULL)
 return (NULL);
 /* treat as empty string */
 len1 = strlen(s1);
+/*calcule longueur chaine s1*/
 len2 = strlen(s2);
+/*calcule longueur chaine s2*/
 result = malloc(sizeof(char) * (len1 + len2 + 1));
+/* allouer memoire pr stsocker chaine resultante */
 if (result == NULL)
+/*verifie si malloc echoue */
 return (NULL);
 strcpy(result, s1);
+/* on copie la chaine s1 dans la memoire allouée */
 strcat(result, s2);
+/* ajoute chaine S2 a la fin*/
 return (result);
 }
-
