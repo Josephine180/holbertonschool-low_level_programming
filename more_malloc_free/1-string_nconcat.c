ecda0a7dc25d7d9feba47f74ff188322a75db281
Josephine180 <pierre-josephine@hotmail.com>
2024-11-11 16:05:30 +0100

    test

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+* string_nconcat - add two strings
+* @s1: string to cat
+* @s2: string to cat
+* @n: taille elt
+* Return: pointer to string or null if failure
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *result;
+/* pointeur */
+unsigned int len1;
+unsigned int len2;
+/*variables qui stocke les longueurs */
+unsigned int i, j;
+/* copient caracteres dans string */
+len1 = strlen(s1);
+len2 = strlen(s2);
+/* renvoie longueur de la string 1 et 2 */
+if (n >= len2)
+n = len2;
+/* si n >S2 on copie pas de caractere en + */
+/*on veut pas dépasser s2 */
+
+result = malloc(((len1 + len2) +1) * sizeof(char));
+if (result == NULL)
+/*verifie si malloc echoue */
+{
+return (NULL);
+}
+for (i = 0; i < len1; i++)
+result[i] = s1[i];
+/* on parcourt tout s1 */
+/* on copie tt dans result */
+/* jusqu'a la fin de s1 */
+/* i parcourt s1 et est copié a la mm position ds result */
+for (j = 0; j < n; j++)
+result[i + j] = s2[j];
+result[i + j] = '\0';
+return (result);
+}
