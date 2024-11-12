4f91d4b09bba9e31dd6516ac105a857aefa7656b
Josephine180 <pierre-josephine@hotmail.com>
2024-11-12 10:52:58 +0100

    d

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
 #define DOG_H
 #ifndef DOG_H
+
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
+ */
 
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
