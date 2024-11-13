9b9584afb6bd53ef18cd98d3dad2ccbc1755c0e5
Josephine180 <pierre-josephine@hotmail.com>
2024-11-13 15:51:32 +0100

    test

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -5,16 +5,17 @@
  * init_dog - Initialise une variable de type struct dog
  * @d: Pointeur vers la structure à initialiser
  * @name: Nom du chien
- * @age: Âge du chien* @owner: Propriétaire du chien
- *
+ * @age: Âge du chien
+ * @owner: proprietaire du chien
  * Retour: Rien (void)
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d != NULL)  /* Vérifier si le pointeur n'est pas NULL */
-    {
-        d->name = name;
-        d->age = age;
-        d->owner = owner;
-    }
+if (d != NULL)
+/* Vérifier si le pointeur n'est pas NULL */
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
 }
