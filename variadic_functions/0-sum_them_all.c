bf000fd2d5d09f6b4e316c682b5d1de6fd9cd94d
Josephine180 <pierre-josephine@hotmail.com>
2024-11-20 16:21:05 +0100

    test

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* sum_them_all - additionne tous les paramètres
+* @n: parametre qui compte les paramètres
+* Return: nombre
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+unsigned int sum = 0;
+va_list args;
+if (n == 0)
+return (0);
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(args, int);
+}
+va_end(args);
+return (sum);
+}
+
---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+* print_numbers - print numbers follow by a new line
+* @separator: pointeur
+* @n: parametre n arguments
+* Return: rien
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list args;
+if (n == 0)
+{
+printf ("n\");
+return;
+}
+va_start(args,n);
+printf("%d", va_arg(args,int));
+for (i = 1; i < n; i++)
+if (separator != NULL)
+{
+printf("%s", separator);
+}
+printf("%d",va_arg(args,int));
+}
+va_end(args);
+printf("\n");
+}
