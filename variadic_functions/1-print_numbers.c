80e923557aa3136332ed3b2512a9f25eaf9af540
Josephine180 <pierre-josephine@hotmail.com>
2024-11-20 16:38:10 +0100

    test

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,6 +15,7 @@ unsigned int i;
 va_list args;
 if (n == 0)
 {
+_putchar("\n");
 return;
 }
 va_start(args,n);
@@ -28,5 +29,5 @@ printf("%s", separator);
 printf("%d",va_arg(args,int));
 }
 va_end(args);
-printf("\n");
+_putchar("\n");
 }
