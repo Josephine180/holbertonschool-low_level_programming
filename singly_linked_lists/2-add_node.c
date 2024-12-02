bb3b3754ec04a77a7f7cf2844242748ff83d43ac
Josephine180 <pierre_josephine@hotmail.com>
2024-12-02 18:16:39 +0100

    test et modifif task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <string.h>
+/**
+* add_node
+* @head:  head/beggining of the list
+* @str: string to store in the list
+* Return: adresse of the head
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *temp;
+size_t nlen;
+temp = malloc(sizeof(list_t)); /* on alloue de la memoire */
+if (temp == NULL) /* on verifie si ça échoue */
+return (NULL);
+temp->str = strdup(str); /* on duplique str */
+for (nlen = 0; str[nlen]; nlen++); /* calcule la longueur de la chaîne */
+temp->len =nlen; /* on affecte la longueur au node */
+temp->next = *head; /* le node suivant est nul car c'est le dernier node */
+*head = temp; /* on commence par le début */
+return (*head);
+}
