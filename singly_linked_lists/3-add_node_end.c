e7c02f4aaf39705d3d00ffadcd5491024470e8f3
Josephine180 <pierre_josephine@hotmail.com>
2024-12-02 17:54:07 +0100

    test

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,46 +11,32 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
- list_t *new_node;      /* Pointer to the new node */
-    list_t *temp = *head;  /* Temporary pointer to traverse the list */
+	list_t *k, *p;
+	size_t nchar;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-    {
-        return (NULL);  /* Return NULL if memory allocation fails */
-    }
+	k = malloc(sizeof(list_t));
+	if (k == NULL)
+		return (NULL);
 
-    /* Duplicate the string and store it in the new node */
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);  /* Free the allocated memory for the node */
-        return (NULL);  /* Return NULL if strdup fails */
-    }
+	k->str = strdup(str);
 
-    /* Set the length of the string */
-    new_node->len = strlen(str);
+	for (nchar = 0; str[nchar]; nchar++)
+		;
 
-    /* The new node's next pointer will be NULL (it's the last node for now) */
-    new_node->next = NULL;
+	k->len = nchar;
+	k->next = NULL;
+	p = *head;
 
-    /* If the list is empty, make the new node the head */
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        /* Otherwise, traverse to the last node */
-        while (temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        /* Link the new node at the end of the list */
-        temp->next = new_node;
-    }
+	if (p == NULL)
+	{
+		*head = k;
+	}
+	else
+	{
+		while (p->next != NULL)
+			p = p->next;
+		p->next = k;
+	}
 
-    /* Return the address of the new node */
-    return (new_node);
+	return (*head);
 }
